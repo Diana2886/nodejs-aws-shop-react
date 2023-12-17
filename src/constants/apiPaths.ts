5aa8a6e3f1514322e533b7d3fd6e10515bf6d043
Diana A <tdm2886@gmail.com>
2023-12-17 12:58:18 +0600

    add changes

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://ic4qlwt2a5.execute-api.eu-west-1.amazonaws.com",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://wo9elbcrc1.execute-api.eu-west-1.amazonaws.com",
+  import: "https://81c6f6is60.execute-api.eu-west-1.amazonaws.com",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
