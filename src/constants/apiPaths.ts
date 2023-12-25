f3796c2ccd8352fdba677549ad9ea874eefa5553
Diana A <tdm2886@gmail.com>
2023-12-25 15:53:41 +0600

    add paths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://ic4qlwt2a5.execute-api.eu-west-1.amazonaws.com",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  order: "https://yziwl0wzv1.execute-api.eu-west-1.amazonaws.com/api",
   import: "https://81c6f6is60.execute-api.eu-west-1.amazonaws.com",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://yziwl0wzv1.execute-api.eu-west-1.amazonaws.com/api",
 };
 
 export default API_PATHS;
