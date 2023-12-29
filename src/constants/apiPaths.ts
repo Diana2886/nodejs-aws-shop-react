e8608b3a03415ada31eb1ebd2a00ab17dcf606eb
Diana A <tdm2886@gmail.com>
2023-12-29 08:20:41 +0600

    change api path for the cart

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: "https://yziwl0wzv1.execute-api.eu-west-1.amazonaws.com/api",
   import: "https://81c6f6is60.execute-api.eu-west-1.amazonaws.com",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://yziwl0wzv1.execute-api.eu-west-1.amazonaws.com/api",
+  cart: "http://diana2886-cart-api-dev.eu-west-1.elasticbeanstalk.com/api",
 };
 
 export default API_PATHS;
